d2f5800ef810cc0ca9c6d5446a4cb7508dc23ddf
Jakub Barabasz <62941129+JakubBarabasz@users.noreply.github.com>
2022-03-24 21:46:17 +0100

    Fasada

---
+++ fasada/src/Fasada.java
@@ -0,0 +1,4 @@
+public interface Fasada {
+    SubSystem01 sub01 = new SubSystem01("John",2,2);
+    SubSystem02 sub02 = new SubSystem02("XYZ", 50);
+}
---
+++ fasada/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Fasada.sub01.getSubSystem01());
+        System.out.println(Fasada.sub02.getSubSystem02());
+
+    }
+}
