60dc6c2c5a05cf2717362949433d08bf3278ab0d
Jakub Barabasz <62941129+JakubBarabasz@users.noreply.github.com>
2022-03-25 08:43:34 +0100

    pylek

---
+++ Pylek/Item.java
@@ -0,0 +1,15 @@
+public class Item {
+    private final String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+
